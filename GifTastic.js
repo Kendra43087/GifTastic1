6ca48ed16e576c3e39bd6f50f4ae4a7f6a74ecbc
Kendra43087 <38871916+Kendra43087@users.noreply.github.com>
2018-06-20 19:06:12 -0400

    Add files via upload

---
+++ GifTastic.js
@@ -0,0 +1,23 @@
+function submitSearch(e) {
+  pageNumber = 0;
+  fetchResults(e);
+}
+
+searchForm.addEventListener('submit', submitSearch)   
+  function fetchResults(e) {
+
+    e.preventDefault();
+
+    url = developers.giphy.com+'?api-key=' + key + '&q='+ searchTerm.value + '&fq=document_type:("image")';
+
+    fetch(url).then(function(result) {
+        return result.json();
+      }).then(function(json) {
+        displayResults(json);
+      });
+    }
+})
+      
+     
+
+  
